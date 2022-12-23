4db966268195f60c7d81c375de8ec8d674c0505d
guilhermeyohan <121273341+guilhermeyohan@users.noreply.github.com>
2022-12-23 01:54:18 -0300

    Add files via upload

--- bkp-shell-script.sh
+++ bkp-shell-script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#Shell script de bkp zipado. Você pode automatizar essa função no crontab.
+#PT-BR Shell script de bkp zipado. Você pode automatizar essa função no crontab.
+#EN-US Backup shell script with zip. You can automate this function in crontab.
 
 $1="CAMINHO DA PASTA DE ORIGEM"
 $2="CAMINNHO DA PASTA DE DESTINO"
@@ -8,5 +9,6 @@ rsync -av --delay-updates "$1" "$2"
 zip -r "$2/backup.zip" "$2"
 
 
-#Lembre-se de dar permissão de execução para o script com o comando chmod +x /path/to/script.sh.
+#PT-BR Lembre-se de dar permissão de execução para o script com o comando chmod +x /path/to/script.sh.
+#EN-US Remember to give execute permission to the script with the command chmod +x /path/to/script.sh.
 
